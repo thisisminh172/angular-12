438dc7f8ce1eb6db125045dba4297b5694d4fc99
thisisminh172 <thisisminh172@gmail.com>
2021-12-15 16:14:33 +0700

    ng if ap dung

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,16 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public name: string = 'Minh Le'
+  public name: string = 'Minh Le';
   public age = 20;
-  public fruits = ['tao','nho','cam','quit','xoai','oi'];
-  constructor() { }
+  public fruits = ['tao', 'nho', 'cam', 'quit', 'xoai', 'oi'];
+  public fruits2 = [
+    { ten: 'Tao', gia: 12, haGia: true },
+    { ten: 'Nho', gia: 30, haGia: false },
+    { ten: 'Cam', gia: -4, haGia: false },
+    { ten: 'Quit', gia: 40, haGia: true },
+  ];
+  constructor() {}
 
   public ngOnInit(): void {
-    console.log('trai cay = ', this.fruits)
+    console.log('trai cay = ', this.fruits);
   }
-
 }
