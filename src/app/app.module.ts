8530959f45c641d3edea8e531f6dbb3f562e5dca
thisisminh172 <thisisminh172@gmail.com>
2021-12-16 11:33:50 +0700

    pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { FormsModule } from '@angular/forms';
+import { OnSalePipe } from './on-sale.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    OnSalePipe
   ],
   imports: [
     BrowserModule,
